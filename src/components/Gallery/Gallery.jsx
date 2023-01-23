365dc2f05b6674e55086e759541a622d49f163b0
ElfNevisokogoRosta <anatoliy.tymoshenko.work@gmail.com>
2023-01-23 13:13:16 +0200

    v1.1.4

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -4,12 +4,13 @@ import { List, Text } from './Gallery.styled';
 import useImagesSearch from 'services/useImagesSearch';
 import Loader from 'components/Loader/Loader';
 import GalleryItem from '../GalleryItem/GalleryItem';
+
 const Gallery = ({ searchQuery, func }) => {
   const [query, setQuery] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   useEffect(() => {
-    setQuery(state => searchQuery);
-  }, []);
+    setQuery(searchQuery);
+  }, [searchQuery]);
   const { loading, error, images, hasMore } = useImagesSearch(
     query,
     pageNumber
